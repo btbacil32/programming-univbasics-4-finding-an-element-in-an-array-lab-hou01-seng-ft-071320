394514b539d63366c3e81404a27d145f348d1aa4
bacil thalji <btbacil32@gmail.com>
2020-07-08 02:36:09 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,3 @@
 def find_element_index(array, value_to_find)
-  valley = [5,4,3,2,1,0,1,2,3,4,5]
-  count = 0
-  while count < valley.length do
-  if valley[count] == value_to_find
-    return value_to_find
-  end
-  count += 1
-  end
+  
 end
