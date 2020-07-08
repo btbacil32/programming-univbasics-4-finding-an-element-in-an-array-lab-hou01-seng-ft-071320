708be71a89c9ffc0d682ee3562d9b0395d07a8b0
bacil thalji <btbacil32@gmail.com>
2020-07-08 03:00:19 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
-  scale_2 = [6,5,4,3]
+  scale = [1,2,3,4]
   value_to_find = 5
   count = 0
   find_value_index = nil
-  while count < scale_2.length do
-    if scale_2[count] == value_to_find
+  while count < scale.length do
+    if scale[count] == value_to_find
       find_value_index = count
     end
     count += 1
